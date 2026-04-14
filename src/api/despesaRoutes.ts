86b1ebe9a8f105c6d8fe13cd9522ddbac05cf227
Raul-Lima77 <rl615220@gmail.com>
2026-04-14 13:41:07 -0300

    att
    attapi

--- src/api/despesaRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-const router = Router()
-
-// ==========================================
-// ROTAS DE DESPESAS
-// ==========================================
-
-export default router
--- src/api/receitaRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-const router = Router()
-
-// ==========================================
-// ROTAS DE RECEITAS
-// ==========================================
-
-export default router
